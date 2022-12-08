f84aede6f9140d65b10c1778d715c393a3dc4e20
Yash Gadhade <yashgadhade@gmail.com>
2022-12-08 11:10:03 +0530

    init

---
+++ scripts/Deployment.ts
@@ -0,0 +1,12 @@
+import { ethers } from "ethers";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+async function main() {
+
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+})
---
+++ tests/Test.ts
@@ -0,0 +1,17 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Basic tests for understanding ERC20", async () => {
+    beforeEach(async () => {
+
+    });
+
+    it("should have zero total supply at deployment", async () => {
+
+    });
+
+    it("triggers the Transfer event with the address of the sender when sending transcation", async function () {
+
+    });
+})
